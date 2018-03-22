c4b11f4c81d1ce835d3f53a9e1afdc62deb61c2d
Rodrigo Fuentes <rdgfuentes+github@gmail.com>
2018-03-22 12:04:01 -0300

    Mapping observable to count cart items

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { ProductService } from './service/product.service';
 import { CartService } from './service/cart.service';
 import { ICartProduct } from './model/icart-product';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+
 
 @Component({
   selector: 'app-root',
@@ -10,18 +13,23 @@ import { ICartProduct } from './model/icart-product';
 })
 export class AppComponent {
   title = 'app';
-  cart: ICartProduct[] = [];
+  cartCount: number;
   displayCart: boolean =  false;
   constructor(
     public productService: ProductService,
     public cartService: CartService
   ) {
-    this.cartService.cart$.subscribe(cart => this.cart = cart );
+    this.countItemsInCart()
+      .subscribe( quantity => {
+        this.cartCount = quantity;
+      });
   }
 
-  countItemsInCart() {
-    const q = this.cart.length > 0 ? this.cart.map( x => x.quantity).reduce( (x, y) => x + y) : 0;
-    return q;
+  countItemsInCart(): Observable<number> {
+    return this.cartService.cart$
+      .map( items => {
+        return items.length > 0 ? items.map( x => x.quantity).reduce( (x, y) => x + y) : 0;
+      });
   }
 
   toggleCart() {
